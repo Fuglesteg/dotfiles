8fcbb364a91985a5123aadb9ffaa2c10344623db
Fuglesteg <andreasfdale@gmail.com>
2023-02-24 14:16:44 +0000

    Highlight current indent

--- nvim/lua/fuglesteg/plugin-config.lua
+++ nvim/lua/fuglesteg/plugin-config.lua
@@ -13,7 +13,11 @@ require("fuglesteg.hydra")
 --     }
 -- })
 
-require("indent_blankline").setup { }
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
 
 require("neogit").setup({
     integrations = {
