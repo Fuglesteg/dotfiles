cf8dfe0c0b05ca53a68d8a1c0bccbef7314c7a5f
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2023-03-04 17:40:57 +0100

    Lua line customize

--- nvim/lua/fuglesteg/plugins/lua-line.lua
+++ nvim/lua/fuglesteg/plugins/lua-line.lua
@@ -1,16 +1,68 @@
--- FIXME: Fix status clock or remove
--- Lua line config
 return {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     config = function()
-        local function statusClock()
+        local function status_clock()
             local timeTable = os.date("*t")
-            local timeHourMinute = timeTable.hour + ":" + timeTable.min
+            local timeHourMinute = (timeTable.hour .. ":" .. timeTable.min) or ""
             return timeHourMinute
         end
-        require('lualine').setup {
-            options = { theme = 'tokyonight' },
-            sections = { lualine_a = { statusClock } }
+
+        local function session_name()
+            return require("possession.session").session_name or ""
+        end
+
+        local theme = require("lualine.themes.tokyonight")
+        local bg_color = theme.visual.b.bg
+        local fg_color = theme.normal.b.fg
+
+        local tabline_value = vim.opt.showtabline
+        require("lualine").setup {
+            options = {
+                theme = "tokyonight",
+                component_separators = "|",
+                section_separators = { left = "", right = "" },
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = {
+                    "diff", "diagnostics",
+                    function()
+                        return "%="
+                    end,
+                    {
+                        "filename",
+                        color = { bg = bg_color, fg = fg_color },
+                        separator = { left = "", right = "" },
+                    },
+                    {
+                        "filetype",
+                        icon_only = true,
+                        color = { bg = bg_color, fg = fg_color },
+                        separator = { left = "", right = "" },
+                    },
+                },
+                lualine_x = {
+                    session_name
+                },
+            },
+            tabline = {
+                lualine_a = {
+                    {
+                        "tabs",
+                        mode = 3,
+                        cond = function()
+                            if #vim.api.nvim_list_tabpages() > 1 then
+                                vim.opt.showtabline = tabline_value
+                                return true
+                            else
+                                vim.opt.showtabline = 0
+                                return false
+                            end
+                        end
+                    },
+                }
+            }
         }
     end,
 }
