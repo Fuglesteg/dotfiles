58f15dfdc775e457dcf13539e83d722716c08ee9
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2023-03-05 21:41:21 +0100

    Add Noice options

--- nvim/lua/fuglesteg/plugins/noice.lua
+++ nvim/lua/fuglesteg/plugins/noice.lua
@@ -1,10 +1,13 @@
 return {
     "folke/noice.nvim",
-    config = function()
-        require("noice").setup({
-            -- add any options here    
-        })
-    end,
+    opts = {
+        presets = {
+            command_palette = true,
+            bottom_search = true,
+            long_message_to_split = true,
+            lsp_doc_border = true,
+        }
+    },
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
