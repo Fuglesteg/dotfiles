4b2ba7d3be750994b33f3f4c9987020b5d6e7b85
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2023-03-17 18:07:14 +0100

    Add diffview config

---
+++ nvim/lua/fuglesteg/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+    'sindrets/diffview.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    opts = {
+        enchanced_diff_hl = true,
+        view = {
+            x = {
+                winbar_info = true,
+
+            }
+        }
+    }
+}
