e1222282cc7121e5f22d35a72d344a035e14d5ee
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2024-10-22 14:39:09 +0200

    Test using SlimDM

--- fuglesteg/system.scm
+++ fuglesteg/system.scm
@@ -44,63 +44,77 @@
                  (kernel-loadable-modules (list v4l2loopback-linux-module))
                  (initrd-modules (cons "kvm_amd" %base-initrd-modules)) ; Virual machine kernel module
                  (services
-                   (append (list
-                             (service kernel-module-loader-service-type
-                                      '("v4l2loopback" "amdgpu" "kvm"))
-                             (service openssh-service-type)
-                             (service cups-service-type)
-                             (service bluetooth-service-type)
-                             (service docker-service-type)
-                             (service containerd-service-type)
-                             (service guix-home-service-type
-                                      `(("andy" ,desktop-home))))
-                           (modify-services %desktop-services
-                                            (gdm-service-type
-                                              config =>
-                                              (gdm-configuration
-                                                (inherit config)
-                                                (xorg-configuration 
-                                                  (xorg-configuration
-                                                    (resolutions '((3840 2160) (1920 1080)))
-                                                    (extra-config '(
-                                                                    "Section \"Device\"
-                                                                    Identifier \"AMD\"
-                                                                    Driver \"amdgpu\"
-                                                                    Option \"TearFree\" \"true\"
-                                                                    Option \"DRI\" \"3\"
-                                                                    Option \"VariableRefresh\" \"true\"
-                                                                    EndSection"))))))
-                                                                    (guix-service-type
-                                                                      config =>
-                                                                      (guix-configuration
-                                                                        (inherit config)
-                                                                        (substitute-urls
-                                                                          (append (list "https://substitutes.nonguix.org")
-                                                                                  %default-substitute-urls))
-                                                                        (authorized-keys
-                                                                          (append (list (plain-file "non-guix.pub" 
-                                                                                                    "(public-key 
-                                                                                                    (ecc 
-                                                                                                      (curve Ed25519)
-                                                                                                      (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
-                                                                                  %default-authorized-guix-keys)))))))
-                                                  (bootloader (bootloader-configuration
-                                                                (bootloader grub-efi-removable-bootloader)
-                                                                (targets (list "/boot/efi"))
-                                                                (keyboard-layout keyboard-layout)))
-                                                  (swap-devices (list (swap-space
-                                                                        (target (uuid
-                                                                                  "e8e45812-542b-4ff3-baac-1fb7b5c34aa3")))))
-                                                  (file-systems (cons* (file-system
-                                                                         (mount-point "/boot/efi")
-                                                                         (device (uuid "49F5-6AE8"
-                                                                                       'fat32))
-                                                                         (type "vfat"))
-                                                                       (file-system
-                                                                         (mount-point "/")
-                                                                         (device (uuid
-                                                                                   "02796792-2e71-4dfa-a014-6daaf49156fc"
-                                                                                   'ext4))
-                                                                         (type "ext4")) %base-file-systems))))
+                   (cons*
+                     (service kernel-module-loader-service-type
+                              '("v4l2loopback" "amdgpu" "kvm"))
+                     (service openssh-service-type)
+                     (service cups-service-type)
+                     (service bluetooth-service-type)
+                     (service docker-service-type)
+                     (service containerd-service-type)
+                     (service guix-home-service-type
+                              `(("andy" ,desktop-home)))
+                     (service slim-service-type (slim-configuration
+                                                  (display ":0")
+                                                  (vt "vt7")
+                                                  (xorg-configuration 
+                                                    (xorg-configuration
+                                                      (resolutions '((3840 2160) (1920 1080)))
+                                                      (extra-config '(
+                                                                      "Section \"Device\"
+                                                                      Identifier \"AMD\"
+                                                                      Driver \"amdgpu\"
+                                                                      Option \"TearFree\" \"true\"
+                                                                      Option \"DRI\" \"3\"
+                                                                      Option \"VariableRefresh\" \"true\"
+                                                                      EndSection"))))))
+                     (service slim-service-type (slim-configuration
+                                                  (display ":0")
+                                                  (vt "vt8")
+                                                  (xorg-configuration 
+                                                    (xorg-configuration
+                                                      (resolutions '((3840 2160) (1920 1080)))
+                                                      (extra-config '(
+                                                                      "Section \"Device\"
+                                                                      Identifier \"AMD\"
+                                                                      Driver \"amdgpu\"
+                                                                      Option \"TearFree\" \"true\"
+                                                                      Option \"DRI\" \"3\"
+                                                                      Option \"VariableRefresh\" \"true\"
+                                                                      EndSection"))))))
+                     (modify-services %desktop-services
+                                      (delete gdm-service-type)
+                                             (guix-service-type
+                                               config =>
+                                               (guix-configuration
+                                                 (inherit config)
+                                                 (substitute-urls
+                                                   (append (list "https://substitutes.nonguix.org")
+                                                           %default-substitute-urls))
+                                                 (authorized-keys
+                                                   (append (list (plain-file "non-guix.pub" 
+                                                                             "(public-key 
+                                                                             (ecc 
+                                                                               (curve Ed25519)
+                                                                               (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+                                                           %default-authorized-guix-keys)))))))
+                             (bootloader (bootloader-configuration
+                                           (bootloader grub-efi-removable-bootloader)
+                                           (targets (list "/boot/efi"))
+                                           (keyboard-layout keyboard-layout)))
+                             (swap-devices (list (swap-space
+                                                   (target (uuid
+                                                             "e8e45812-542b-4ff3-baac-1fb7b5c34aa3")))))
+                             (file-systems (cons* (file-system
+                                                    (mount-point "/boot/efi")
+                                                    (device (uuid "49F5-6AE8"
+                                                                  'fat32))
+                                                    (type "vfat"))
+                                                  (file-system
+                                                    (mount-point "/")
+                                                    (device (uuid
+                                                              "02796792-2e71-4dfa-a014-6daaf49156fc"
+                                                              'ext4))
+                                                    (type "ext4")) %base-file-systems))))
 
 k80
