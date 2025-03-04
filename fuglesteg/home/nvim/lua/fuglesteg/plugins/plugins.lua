e6aa91bd6dc37d08037f56754cd3aafd40982364
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-03-04 17:03:32 +0100

    Nvim: Remove nvim-dap-go

--- fuglesteg/home/nvim/lua/fuglesteg/plugins/plugins.lua
+++ fuglesteg/home/nvim/lua/fuglesteg/plugins/plugins.lua
@@ -1,24 +1,19 @@
 return {
--- Code intelligence
-	"leoluz/nvim-dap-go",
+    -- Formatting
+    "tpope/vim-sleuth",
 
--- Formatting
-	"tpope/vim-sleuth",
-
--- Utility
+    -- Utility
     'amadeus/vim-convert-color-to',
     'tpope/vim-surround',
     'tpope/vim-commentary',
     'mrjones2014/smart-splits.nvim',
-    'voldikss/vim-floaterm',
 
--- Keybinds
+    -- Keybinds
     'folke/which-key.nvim',
 
--- Visual
-    'onsails/lspkind.nvim',
+    -- Visual
     'kyazdani42/nvim-web-devicons',
 
--- Colorschemes
+    -- Colorschemes
     'romgrk/doom-one.vim',
 }
