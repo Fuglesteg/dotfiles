19164cc8cb04f1bf7e408b37e3c6adfa84ac0430
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-03-04 17:04:38 +0100

    Nvim: Add DAP configuration for mason and special config for NIF

--- fuglesteg/home/nvim/lua/fuglesteg/plugins/dap.lua
+++ fuglesteg/home/nvim/lua/fuglesteg/plugins/dap.lua
@@ -1,7 +1,37 @@
 return {
     "mfussenegger/nvim-dap",
+    dependencies = { "jay-babu/mason-nvim-dap.nvim", "williamboman/mason.nvim" },
     config = function()
         local dap = require("dap")
-        require("dap-go").setup()
-    end,
+        require("mason-nvim-dap").setup({
+            handlers = {
+                function(config)
+                    -- Keep original functionality
+                    require('mason-nvim-dap').default_setup(config)
+                end,
+                firefox = function(config)
+                    config.configurations = {
+                        {
+                            name = 'Firefox: Debug',
+                            type = 'firefox',
+                            request = 'launch',
+                            reAttach = true,
+                            firefoxExecutable = vim.fn.exepath('firefox'),
+                            url = "dev.minidrett.no",
+                            webRoot = "${workspaceFolder}/src",
+                            pathMappings = {
+                                {
+                                    url = "webpack://nif/${workspaceFolderBasename}/src",
+                                    path = "${webRoot}"
+                                }
+                            },
+                            sourceMaps = true
+                        }
+                    }
+                    config.filetypes = 	{ 'javascriptreact', 'typescriptreact', 'typescript', 'javascript', 'vue' }
+                    require('mason-nvim-dap').default_setup(config) -- don't forget this!
+                end,
+            }
+        })
+    end
 }
